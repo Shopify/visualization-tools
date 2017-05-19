57fb8defbd79bc3db18b29dcf69486e46409adc7
Kevin Charette <kevin.charette@shopify.com>
2017-05-19 16:23:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="funnel-tree-visualization",
+    name="funnel_tree_visualization",
     version="0.1.0",
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    package_dir={'funnel-tree-visualization': 'funnel-tree-visualization'}
+    package_dir={'funnel_tree_visualization': 'funnel_tree_visualization'}
 )
