0328b400085e0cf123407e67e3feb5646745175e
Kevin Charette <kevin.charette@shopify.com>
2017-05-21 23:40:25 -0400

    Changing back the repo to the right name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="funnel",
+    name="funnel_tree_vis",
     version="0.1.0",
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    package_dir={'funnel': 'funnel'}
+    package_dir={'funnel_tree_vis': 'funnel_tree_vis'}
 )
